7a6fee5e92fb104b7dbf4711214abf5d903a77b1
Ngoc Pham <tokanizar@gmail.com>
2016-01-13 22:40:58 -0500

    Improve file opening & checking for when to send email

--- reporter.php
+++ reporter.php
@@ -8,28 +8,37 @@ use Mailgun\Mailgun;
 try {
     $config = loadConfig();
 
-    $currentIp = trim(file_get_contents('ip.txt'));
+    $handle = fopen('ip.txt', 'c+');
+    if ($handle === false) return 1;
+
+    $currentIp = trim(stream_get_contents($handle));
     $ip = trim(file_get_contents('http://i.ngx.cc'));
 
     // If the IP checked different from the current IP logged (to file). Send
     // notification
     if ($currentIp !== $ip) {
-        file_put_contents('ip.txt', $ip);
+        fwrite($handle, $ip);
 
-        $mg = new Mailgun($config['mailgun_api_key']);
+        if ($currentIp !== '') {
+            $mg = new Mailgun($config['mailgun_api_key']);
 
-        $mg->sendMessage(
-            $config['mailgun_domain'],
-            [
-                'from'    => $config['report_from_email'],
-                'to'      => $config['report_to_email'],
-                'subject' => 'Home IP has changed',
-                'text'    => 'Ngoc, your new IP is ' . $ip
-            ]
-        );
+            $mg->sendMessage(
+                $config['mailgun_domain'],
+                [
+                    'from'    => $config['report_from_email'],
+                    'to'      => $config['report_to_email'],
+                    'subject' => 'Home IP has changed',
+                    'text'    => 'Ngoc, your new IP is ' . $ip
+                ]
+            );
+        }
     }
 } catch (Exception $e) {
     echo $e->getMessage();
     return 1;
+} finally {
+    if ($handle !== null) {
+        fclose($handle);
+    }
 }
 
