e8e81430ca22b8dbe28d5d1db4eb3e2846fecd5d
Ngoc Pham <ngocphamm@users.noreply.github.com>
2018-03-15 20:18:47 -0400

    Add sample config file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+function loadConfig()
+{
+    return [
+        'cloudflare_api_key' => '',
+        'cloudflare_email' => '',
+        'cloudflare_zone_id' => '',
+        'cloudflare_domain_id' => '',
+        'cloudflare_domain_name' => '',
+        'mailgun_api_key' => '',
+        'mailgun_domain' => '',
+        'report_from_email' => '',
+        'report_to_email' => ''
+    ];
+}
