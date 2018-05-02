6d0ddeac912fd33944c6434e47b717f8234ebf65
Ngoc Pham <tokanizar@gmail.com>
2018-05-02 15:36:22 -0400

    Use Guzzle for API request

--- reporter.php
+++ reporter.php
@@ -1,20 +1,21 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/config.php';
 
-use Httpful\Request;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use Mailgun\Mailgun;
+use Psr\Http\Message\ResponseInterface;
 
 try {
-    $config = loadConfig();
+    $config = require_once __DIR__ . '/config.php';
 
     $db = new PDO('sqlite:' . __DIR__ . '/ip.db3');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $stmt = $db->query('SELECT * FROM ip ORDER BY added_at DESC LIMIT 1');
     $stmt->execute();
-    $current_ip = $stmt->fetch(PDO::FETCH_ASSOC);
+    $currentIp = $stmt->fetch(PDO::FETCH_ASSOC);
 
     $ip = trim(file_get_contents('https://i.ngx.cc'));
 
@@ -22,30 +23,33 @@ try {
 
     // If the IP checked different from the current IP logged (to file), update
     // CloudFlare DNS record for and send and email notification
-    if ($current_ip === false || $current_ip['ip'] !== $ip) {
+    if ($currentIp === false || $currentIp['ip'] !== $ip) {
         // Add ip to database
         $stmt = $db->prepare('INSERT INTO ip (ip, added_at, check_count) VALUES (:ip, :dt, 1)')
                     ->execute([ ':ip' => $ip, ':dt' => date('Y-m-d H:i:s') ]);
 
         // Update CloudFlare DNS using API call
-        $cfPutData = [
-            'content' => $ip,
-            'type' => 'A',
-            'name' => $config['cloudflare_domain_name']
-        ];
-
-        $response = Request::put("https://api.cloudflare.com/client/v4/zones/{$config['cloudflare_zone_id']}/dns_records/{$config['cloudflare_domain_id']}")
-            ->sendsJson()
-            ->addHeaders(array(
+        $client = new Client([ 'base_uri' => 'https://api.cloudflare.com/client/v4/zones/' ]);
+        $promise = $client->requestAsync('PUT', "{$config['cloudflare_zone_id']}/dns_records/{$config['cloudflare_domain_id']}", [
+            'headers' => [
                 'X-Auth-Key' => $config['cloudflare_api_key'],
                 'X-Auth-Email' => $config['cloudflare_email']
-            ))
-            ->body(json_encode($cfPutData))
-            ->send();
-
-        if ($response->code !== 200) {
-            throw new Exception('Attempt to update CloudFlare DNS record failed!');
-        }
+            ],
+            'json' => [
+                'content' => $ip,
+                'type' => 'A',
+                'name' => $config['cloudflare_domain_name']
+            ]
+        ])->then(
+            function (ResponseInterface $res) {
+                if ($res->getStatusCode() !== 200) {
+                    throw new Exception("Attempt to update CloudFlare DNS record failed! Status code: {$res->getStatusCode()}");
+                }
+            },
+            function (RequestException $e) {
+                throw new Exception("Attempt to update CloudFlare DNS record failed! Message: {$e->getMessage()}");
+            }
+        )->wait();
 
         // Send email using Mailgun
         $mg = Mailgun::create($config['mailgun_api_key']);
@@ -54,14 +58,14 @@ try {
             'from'    => $config['report_from_email'],
             'to'      => $config['report_to_email'],
             'subject' => 'Home IP has changed',
-            'text'    => 'Ngoc, your new IP is ' . $ip . ' Old IP: ' . $current_ip['ip']
+            'text'    => "New IP: {$ip}\nOld IP: {$currentIp['ip']}"
         ]);
     } else {
         // Update check count
         $db->prepare('UPDATE ip SET check_count = :cc WHERE id = :id')
             ->execute([
-                ':cc' => $current_ip['check_count'] + 1,
-                ':id' => intval($current_ip['id'])
+                ':cc' => $currentIp['check_count'] + 1,
+                ':id' => intval($currentIp['id'])
             ]);
     }
 } catch (Exception $e) {
@@ -72,4 +76,3 @@ try {
         $db = null;
     }
 }
-
--- sample_config.php
+++ sample_config.php
@@ -1,16 +1,15 @@
 <?php
 
-function loadConfig()
-{
-    return [
-        'cloudflare_api_key' => '',
-        'cloudflare_email' => '',
-        'cloudflare_zone_id' => '',
-        'cloudflare_domain_id' => '',
-        'cloudflare_domain_name' => '',
-        'mailgun_api_key' => '',
-        'mailgun_domain' => '',
-        'report_from_email' => '',
-        'report_to_email' => ''
-    ];
-}
+$config = [
+    'cloudflare_api_key' => '',
+    'cloudflare_email' => '',
+    'cloudflare_zone_id' => '',
+    'cloudflare_domain_id' => '',
+    'cloudflare_domain_name' => '',
+    'mailgun_api_key' => '',
+    'mailgun_domain' => '',
+    'report_from_email' => '',
+    'report_to_email' => ''
+];
+
+return $config;
