0b579ee33b37935ad17f4e7151ea06328cfb1220
Ngoc Pham <ngocphamm@users.noreply.github.com>
2020-09-24 18:52:50 -0400

    Update sample config to use CloudFlare API Token

--- sample_config.php
+++ sample_config.php
@@ -4,15 +4,15 @@
 // CREATE TABLE "ip"(id integer NOT NULL PRIMARY KEY AUTOINCREMENT, ip text not null, check_count integer not null, added_at text not null, last_updated text);
 
 $config = [
-    'cloudflare_api_key' => '',
-    'cloudflare_email' => '',
-    'cloudflare_zone_id' => '',
-    'cloudflare_domain_id' => '',
+    'cloudflare_api_token'   => '',
+    'cloudflare_email'       => '',
+    'cloudflare_zone_id'     => '',
+    'cloudflare_domain_id'   => '',
     'cloudflare_domain_name' => '',
-    'mailgun_api_key' => '',
-    'mailgun_domain' => '',
-    'report_from_email' => '',
-    'report_to_email' => ''
+    'mailgun_api_key'        => '',
+    'mailgun_domain'         => '',
+    'report_from_email'      => '',
+    'report_to_email'        => ''
 ];
 
 return $config;
